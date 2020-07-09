b93abbfeb7cce7f007650c51d266a05f89df1fc1
Tibes <tibes93@hotmail.com>
2020-07-09 16:35:13 -0300

    pare de chutar CSS finalizado

--- curso-JS/exercicio004/script.js
+++ curso-JS/exercicio004/script.js
@@ -5,25 +5,25 @@ buttonElement.addEventListener("click", function(){
 })
 
 function calcular(){
-    var elementStart = document.querySelector('input#start')
-    var elementEnd = document.querySelector('input#end')
-    var elementSpeed = document.querySelector('input#speed')
+    let elementStart = document.querySelector('input#start')
+    let elementEnd = document.querySelector('input#end')
+    let elementSpeed = document.querySelector('input#speed')
 
 
-    var start = Number(elementStart.value)
-    var end = Number(elementEnd.value)
-    var speed = Number(elementSpeed.value)
+    let start = Number(elementStart.value)
+    let end = Number(elementEnd.value)
+    let speed = Number(elementSpeed.value)
 
         if(start == 0 || end== 0 || speed==0){
             window.alert(`[ERRO] Faltam dados`)
         } else if (start<end){
             contagem.innerHTML='contagem: '
-            for (var c = start ; c<=end ; c=c+speed){
+            for (let c = start ; c<=end ; c=c+speed){
                 contagem.innerHTML+=` \u{1F449} ${c} `
         }
         } else if(start>end) {
             contagem.innerHTML='contagem: '
-            for (var c = start ; c>=end ; c=c-speed){
+            for (let c = start ; c>=end ; c=c-speed){
                 contagem.innerHTML+=` \u{1F449} ${c}`
         }       
     }
